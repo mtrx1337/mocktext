96af60d6b27cfe846cc44e0c041b679d531e941d
Leonard Lorenz <bi0m3tr1xx@gmail.com>
2019-09-30 10:42:26 +0200

    implemented help text and reverse case

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,46 @@
 use std::io::{self, BufRead};
+use std::env;
+
 
 fn main() {
-    let stdin = io::stdin();
+    let mut reversed : bool = false;
+    let args : Vec<String> = env::args().collect();
+    if args.len() < 1 {
+        read_from_stdin(reversed);
+    } else {
+        for arg in args {
+            match arg.as_str() {
+                "-h" => show_help(), // print help
+                "-r" => {
+                    reversed = true;
+                    read_from_stdin(reversed)
+                }, // reversed character casing
+                _ => (),
+            }
+        }
+    }
+}
 
+fn read_from_stdin(reversed : bool) {
+    let stdin = io::stdin();
     for line in stdin.lock().lines() {
         let mut output_line = vec![];
         let mut i = 0;
 
         for character in line.unwrap().chars() {
+
             if i % 2 == 0 {
-                output_line.push(character.to_ascii_uppercase())
+                if reversed {
+                    output_line.push(character.to_ascii_lowercase())
+                } else {
+                    output_line.push(character.to_ascii_uppercase())
+                }
             } else {
-                output_line.push(character.to_ascii_lowercase())
+                if reversed {
+                    output_line.push(character.to_ascii_uppercase())
+                } else {
+                    output_line.push(character.to_ascii_lowercase())
+                }
             }
             i += 1;
         }
@@ -21,4 +50,17 @@ fn main() {
         }
         print!("\n");
     }
+
+}
+
+fn show_help() {
+    println!("\
+SYNOPSIS:
+\tEither hand over text via standard in:
+\t\techo 'text' | mocktext\n
+\tOr execute mocktext on it's own and just type text into the terminal:
+\t\t$ mocktext
+\t\t> techno isn't real music
+\t\t  TeChNo iSn'T ReAl mUsIc
+");
 }
